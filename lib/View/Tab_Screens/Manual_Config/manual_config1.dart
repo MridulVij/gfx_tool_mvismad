6b7ddfc753ec74bafce376780cfdfad90de0b531
mridul <mridulvij1569@gmail.com>
2023-07-02 23:58:23 +0530

    code added

---
+++ lib/View/Tab_Screens/Manual_Config/manual_config1.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ManualConfig extends StatefulWidget {
+  const ManualConfig({super.key});
+
+  @override
+  State<ManualConfig> createState() => _ManualConfigState();
+}
+
+class _ManualConfigState extends State<ManualConfig> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gfx_tool_mvismad/View/Tab_Screens/Manual_Config/manual_config1.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,11 +11,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: ManualConfig(),
     );
   }
 }
